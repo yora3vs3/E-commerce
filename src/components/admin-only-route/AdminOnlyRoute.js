bb395a29bb4e880a71e4c4143cf180e56b0ea9de
Boaz <boaserem022@gmail.com>
2023-06-03 07:38:29 +0300

    added the admin section

---
+++ src/components/admin-only-route/AdminOnlyRoute.js
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import { selectEmail } from "../../redux/slice/authSlice";
+
+const AdminOnlyRoute = ({ children }) => {
+  const userEmail = useSelector(selectEmail);
+
+  if (userEmail === "test@gmail.com") {
+    return children;
+  }
+  return null;
+};
+
+export default AdminOnlyRoute;
