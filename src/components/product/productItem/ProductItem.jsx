1101104f56f0798d2c3f7cf519cd8351541210bc
Boaz <boaserem022@gmail.com>
2023-06-06 09:32:15 +0300

    updated product listing page

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../card/Card";
+import styles from "./ProductItem.module.scss";
 
-const ProductItem = () => {
+const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat("...");
+      return shortenedText;
+    }
+    return text;
+  };
+
   return (
-    <div>ProductItem</div>
-  )
-}
+    <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
+      <Link to={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <img src={imageURL} alt={name} />
+        </div>
+      </Link>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>{`$${price}`}</p>
+          <h4>{shortenText(name, 18)}</h4>
+        </div>
+        {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
 
-export default ProductItem
+        <button className="--btn --btn-danger">Add To Cart</button>
+      </div>
+    </Card>
+  );
+};
+
+export default ProductItem;
