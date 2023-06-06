0906f7e8ff0d078b6202e29d46a74156701f2e9f
Boaz <boaserem022@gmail.com>
2023-06-06 16:19:33 +0300

    updated filtered products

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore} from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import productReducer from "./slice/productSlice";
+import filterReducer from "./slice/filterSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
+    filter: filterReducer,
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
