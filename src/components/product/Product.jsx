40cc7ab5835e57aacaba0e49b824b6982b159681
Boaz <boaserem022@gmail.com>
2023-06-06 04:51:42 +0300

    added useFetch collection hook

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./Product.module.scss";
+import ProductFilter from "./productFilter/ProductFilter";
+import ProductList from "./productList/ProductList";
 
 const Product = () => {
   return (
-    <div>Product</div>
-  )
-}
+    <section>
+      <div className={`container ${styles.product}`}>
+        <aside className={styles.filter}>
+          <ProductFilter />
+        </aside>
+        <div className={styles.content}>
+          <ProductList />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Product
+export default Product;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 // import AdminOnlyRoute from "../../components/adminOnlyRoute/AdminOnlyRoute";
 import Slider from "../../components/slider/Slider";
+import Product from "../../components/product/Product";
 
 const Home = () => {
   return (
     <div>
       {/* <Slider /> */}
-      <h1>Home Page</h1>
+    <Product/>
     </div>
   );
 };
