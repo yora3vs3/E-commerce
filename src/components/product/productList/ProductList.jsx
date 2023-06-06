cd8bfb635726d92aedef1be037d920738aea513a
Boaz <boaserem022@gmail.com>
2023-06-06 05:25:46 +0300

    added product fillllter

--- src/components/product/productList/ProductList.jsx
+++ src/components/product/productList/ProductList.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./ProductList.module.scss";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import Search from "../../search/Search";
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
 
-export default ProductList
+  return (
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+
+          <p>
+            <b>10</b> Products found.
+          </p>
+        </div>
+        {/* Search Icon */}
+        <div>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+        {/* Sort Products */}
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
