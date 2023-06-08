60110a5063612ff794fb2334d5ff2ff48dd3815e
Boaz <boaserem022@gmail.com>
2023-06-08 15:27:44 +0300

    added checkout details page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Header, Footer } from "./components";
 import AdminOnlyRoute from "./components/adminOnlyRoute/AdminOnlyRoute";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
 import Cart from "./pages/cart/Cart";
-// import CheckoutDetails from "./pages/checkout/CheckoutDetails";
+import CheckoutDetails from "./pages/checkout/CheckoutDetails";
 // import Checkout from "./pages/checkout/Checkout";
 // import CheckoutSuccess from "./pages/checkout/CheckoutSuccess";
 // import OrderHistory from "./pages/orderHistory/OrderHistory";
@@ -39,13 +39,13 @@ function App() {
 
           <Route path="/product-details/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/checkout-details" element={<CheckoutDetails />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/checkout-success" element={<CheckoutSuccess />} />
-          <Route path="/order-history" element={<OrderHistory />} />
-          <Route path="/order-details/:id" element={<OrderDetails />} />
-          <Route path="/review-product/:id" element={<ReviewProducts />} />
-           */}
+          <Route path="/checkout-details" element={<CheckoutDetails />} />
+          {/* <Route path="/checkout" element={<Checkout />} /> */}
+          {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
+          {/* <Route path="/order-history" element={<OrderHistory />} /> */}
+          {/* <Route path="/order-details/:id" element={<OrderDetails />} /> */}
+          {/* <Route path="/review-product/:id" element={<ReviewProducts />} /> */}
+          
         </Routes>
         <Footer />
       </BrowserRouter>
