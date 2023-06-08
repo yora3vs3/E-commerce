9e149329b25e11931f7fac5dffc5345e69e303db
Boaz <boaserem022@gmail.com>
2023-06-08 11:17:28 +0300

    added redirect to login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -5,102 +5,112 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Card from "../../components/card/Card";
 import {
-    GoogleAuthProvider,
-    signInWithEmailAndPassword,
-    signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { toast } from "react-toastify";
 import Loader from "../../components/loader/Loader";
+import { useSelector } from "react-redux";
+import { selectPreviousURL } from "../../redux/slice/cartSlice";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const navigate = useNavigate();
+  const previousURL = useSelector(selectPreviousURL);
+  const navigate = useNavigate();
 
-    const loginUser = (e) => {
-        e.preventDefault();
-        setIsLoading(true);
+  const redirectUser = () => {
+    if (previousURL.includes("cart")) {
+      return navigate("/cart");
+    }
+    navigate("/");
+  };
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setIsLoading(false);
-                toast.success("Login Successful...");
-                navigate("/");
-            })
-            .catch((error) => {
-                setIsLoading(false);
-                toast.error(error.message);
-            });
-    };
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-    // Login with Goooglr
-    const provider = new GoogleAuthProvider();
-    const signInWithGoogle = () => {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                const user = result.user;
-                toast.success("Login Successfully");
-                navigate("/");
-            })
-            .catch((error) => {
-                toast.error(error.message);
-            });
-    };
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // const user = userCredential.user;
+        setIsLoading(false);
+        toast.success("Login Successful...");
+        redirectUser();
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
 
-    return (
-        <>
-            {isLoading && <Loader />}
-            <section className={`container ${styles.auth}`}>
-                <div className={styles.img}>
-                    <img src={loginImg} alt="Login" width="400" />
-                </div>
+  // Login with Goooglr
+  const provider = new GoogleAuthProvider();
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // const user = result.user;
+        toast.success("Login Successfully");
+        redirectUser();
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
-                <Card>
-                    <div className={styles.form}>
-                        <h2>Login</h2>
+  return (
+    <>
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt="Login" width="400" />
+        </div>
 
-                        <form onSubmit={loginUser}>
-                            <input
-                                type="text"
-                                placeholder="Email"
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                required
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <button type="submit" className="--btn --btn-primary --btn-block">
-                                Login
-                            </button>
-                            <div className={styles.links}>
-                                <Link to="/reset">Reset Password</Link>
-                            </div>
-                            <p>-- or --</p>
-                        </form>
-                        <button
-                            className="--btn --btn-danger --btn-block"
-                            onClick={signInWithGoogle}
-                        >
-                            <FaGoogle color="#fff" /> Login With Google
-                        </button>
-                        <span className={styles.register}>
-                            <p>Don't have an account?</p>
-                            <Link to="/register">Register</Link>
-                        </span>
-                    </div>
-                </Card>
-            </section>
-        </>
-    );
+        <Card>
+          <div className={styles.form}>
+            <h2>Login</h2>
+
+            <form onSubmit={loginUser}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Login
+              </button>
+              <div className={styles.links}>
+                <Link to="/reset">Reset Password</Link>
+              </div>
+              <p>-- or --</p>
+            </form>
+            <button
+              className="--btn --btn-danger --btn-block"
+              onClick={signInWithGoogle}
+            >
+              <FaGoogle color="#fff" /> Login With Google
+            </button>
+            <span className={styles.register}>
+              <p>Don't have an account?</p>
+              <Link to="/register">Register</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
+  );
 };
 
 export default Login;
