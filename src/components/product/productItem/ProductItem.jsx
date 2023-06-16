320d9440cac95e02581f0039d50916eb8bad0ad7
Boaz <boaserem022@gmail.com>
2023-06-16 12:39:14 +0300

    updated product item

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import {
@@ -10,6 +10,7 @@ import styles from "./ProductItem.module.scss";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
   const dispatch = useDispatch();
+  const [showBtn, setShowBtn] = useState(false);
   const shortenText = (text, n) => {
     if (text.length > n) {
       const shortenedText = text.substring(0, n).concat("...");
@@ -24,27 +25,33 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
   };
 
   return (
-    <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
-      <Link to={`/product-details/${id}`}>
-        <div className={styles.img}>
-          <img src={imageURL} alt={name} />
-        </div>
-      </Link>
-      <div className={styles.content}>
-        <div className={styles.details}>
-          <p>{`$${price}`}</p>
-          <h4>{shortenText(name, 18)}</h4>
-        </div>
-        {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
+    <div 
+    className={styles.container}
+    onMouseLeave={() => setShowBtn(false)} 
+    onMouseEnter={() => setShowBtn(true)}>
 
-        <button
-          className="--btn --btn-danger"
-          onClick={() => addToCart(product)}
-        >
-          Add To Cart
-        </button>
-      </div>
-    </Card>
+      <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
+        <Link to={`/product-details/${id}`}>
+          <div className={styles.img}>
+            <img src={imageURL} alt={name} />
+          </div>
+        </Link>
+        <div className={styles.content}>
+          <div className={styles.details}>
+            <p>{`$${price}`}</p>
+            <h4>{shortenText(name, 18)}</h4>
+          </div>
+          {!grid && <p className={styles.desc}>{shortenText(desc, 300)}</p>}
+
+          {showBtn && <button
+            className="--btn --btn-danger"
+            onClick={() => addToCart(product)}
+          >
+            Add To Cart
+          </button>}
+        </div>
+      </Card>
+    </div>
   );
 };
 
