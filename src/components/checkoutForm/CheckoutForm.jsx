79729a5ce078c8fed3c9a77b5474b5b4efe82d59
Boaz <boaserem022@gmail.com>
2023-06-17 12:27:13 +0300

    updated endoin

--- src/components/checkoutForm/CheckoutForm.jsx
+++ src/components/checkoutForm/CheckoutForm.jsx
@@ -91,7 +91,7 @@ const CheckoutForm = () => {
                 elements,
                 confirmParams: {
                     // Make sure to change this to your payment completion page
-                    return_url: "http://localhost:3000/checkout-success",
+                    return_url: "https://wazibiz.vercel.app/checkout-success",
                 },
                 redirect: "if_required",
             })
